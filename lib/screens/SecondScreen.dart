de1c9d0123f831e07ee409bf7e8492f59b35fdce
Vojtěch Pšenák <psenak.vojtech@gmail.com>
2021-03-30 14:23:48 +0200

    text saving

--- lib/screens/SecondScreen.dart
+++ lib/screens/SecondScreen.dart
@@ -1,6 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class SecondScreen extends StatelessWidget {
+class SecondScreen extends StatefulWidget {
+  SecondScreen({Key key}) : super(key: key);
+
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+
+class _SecondScreenState extends State<SecondScreen> {
+  TextEditingController _controller;
+  String _savedText = "";
+  SharedPreferences prefs;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+    SharedPreferences.getInstance().then((value) {
+      prefs = value;
+      setState(() {
+        _savedText = prefs.getString("textFromSecondScreen");
+      });
+    });
+  }
+
+  void _saveText() async {
+    await prefs.setString("textFromSecondScreen", _controller.text);
+    _controller.clear();
+  }
+
+  void _reload() async {
+    setState(() {
+      _savedText = prefs.getString("textFromSecondScreen");
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +46,22 @@ class SecondScreen extends StatelessWidget {
       body: Center(
           child: Column(
             children: [
-              Row(
+              Column(
                 children: [
-                  Text("Druhá obrazovka"),
+                  TextField(
+                    controller: _controller,
+                  ),
+                  ElevatedButton(child: Text("Uložit"), onPressed: _saveText),
+                ],
+              ),
+              Column(
+                children: [
+                  Text("Naposledy jste napsal:"),
+                  Text(_savedText),
+                  ElevatedButton(
+                    child: Text("Načíst"),
+                    onPressed: _reload,
+                  ),
                   ElevatedButton(
                     child: Text("Zpět na hlavní obrazovku"),
                     onPressed: () {
