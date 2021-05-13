f902a2de365a6cd3e1b0f50f13b47bbbb60fb7ef
Karlos1235 <karol.novy123@seznam.cz>
2021-05-13 15:35:28 +0200

    Karlos

---
+++ lib/components/components.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Components {
+  Drawer getDrawer(BuildContext context) {
+    return Drawer(
+      // Add a ListView to the drawer. This ensures the user can scroll
+      // through the options in the drawer if there isn't enough vertical
+      // space to fit everything.
+      child: ListView(
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            child: Text('Menu'),
+            decoration: BoxDecoration(
+              color: Colors.green,
+            ),
+          ),
+          ListTile(
+            title: Text('Zápisník'),
+
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.pushNamed(context,'/second');
+            },
+          ),
+          ListTile(
+            title: Text('Kamera'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.pushNamed(context,'/camera');
+            },
+          ),
+          ListTile(
+            title: Text('Fotky'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.pushNamed(context,'/image');
+            },
+          ),
+        ],
+      ),
+
+    );
+  }
+
+  FloatingActionButton getHomeButton(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () => Navigator.pushNamed(context, "/"),
+      tooltip: '/',
+      child: const Icon(Icons.home),
+    );
+  }
+
+
+
+}
