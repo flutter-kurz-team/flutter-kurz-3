8215e3f721929f7b28d08c7da8210838ccb910dd
Ondra Soldát <darnexus.04@gmail.com>
2021-05-13 10:32:06 +0200

    Update homeFloatingButton.dart
    Nahrazení HomeScreen => WelcomeScreen

--- lib/components/homeFloatingButton.dart
+++ lib/components/homeFloatingButton.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 FloatingActionButton getHomeButton(BuildContext context) {
   return FloatingActionButton(
-    onPressed: () => Navigator.pushNamed(context, "/"),
-    tooltip: '/',
+    onPressed: () => Navigator.pushNamed(context, "/welcome"),
+    tooltip: '/welcome',
     child: const Icon(Icons.home),
   );
 }
