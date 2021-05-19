f736b8c0efe3d4aecbbf445a29763c627dbff215
Karlos1235 <karol.novy123@seznam.cz>
2021-05-19 10:24:25 +0200

    NewsScreen

---
+++ lib/screens/NewsScreen.dart
@@ -0,0 +1,75 @@
+class NewsScreen extends StatefulWidget {
+  NewsScreen({Key key}) : super(key: key);
+
+  @override
+  _NewsScreenState createState() => _NewsScreenState();
+}
+
+class _NewsScreenState extends State<NewsScreen> {
+  SharedPreferences _prefs;
+  bool started = false;
+  String feedUrl = "https://servis.idnes.cz/rss.aspx?c=zpravodaj";
+
+  void initState() {
+    super.initState();
+    SharedPreferences.getInstance().then((value) {
+      _prefs = value;
+      started = true;
+      print("Nastartováno");
+    });
+  }
+
+  Future getFeed() async {
+    var url = Uri.parse(feedUrl);
+    var response = await http.get(url);
+    var rssFeed = RssFeed.parse(response.body);
+    return rssFeed;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new WillPopScope(onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Novinky"),
+          automaticallyImplyLeading: false,
+          actions: [
+          ],
+        ),
+        body: FutureBuilder(
+            future: getFeed(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                //snapshot.data == rssFeed
+                return Column(
+                  children: [
+                    Text(snapshot.data.title),
+                    Container(
+                      height: 500,
+                      child:
+                      ListView.builder(
+                          itemCount: snapshot.data.items.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            var curItem = snapshot.data.items[index];
+
+                            return Column(
+                              children: [
+                                Text(curItem.title),
+                                Text(curItem.description),
+                                Container(height: 10),
+                              ],
+                            );
+                          }
+                      ),
+                    ),
+                  ],
+                );
+              }
+              return CircularProgressIndicator();
+            }
+        ),
+        floatingActionButton: getHomeButton(context),
+      ),
+    );
+  }
+}
